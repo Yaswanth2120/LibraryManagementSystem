a1c546019a730d7bf2e9073acdc74b8ffd720c1d
Yaswanth <yaswanthsai2120@gmail.com>
2025-04-27 22:57:37 -0500

    Fixed some bugs

--- lms-frontend/src/pages/RegisterPage.jsx
+++ lms-frontend/src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../services/api.js";
-import { useAuth } from "../context/AuthContext";
+import  useAuth from "../context/useAuth.js"
 
 function RegisterPage() {
   const { login } = useAuth();
